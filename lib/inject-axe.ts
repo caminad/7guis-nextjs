10babbb71aa2a5c0ffaadae940143c95d1f0f888
David Jones <david@omg.lol>
2022-06-11 23:02:03 +0100

    Add axe-core

---
+++ lib/inject-axe.ts
@@ -0,0 +1,5 @@
+import axe from '@axe-core/react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+void axe(React, ReactDOM, 1000)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 import '../styles/main.scss'
 
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  void import('../lib/inject-axe')
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
