e59aaadc85020f1116070404ffc3abb952a6f9d4
David Jones <david@omg.lol>
2022-06-15 19:50:36 +0100

    Remove leftover axe development module

--- lib/axe.development.ts
+++
@@ -1,5 +0,0 @@
-import reactAxe from '@axe-core/react'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-void reactAxe(React, ReactDOM, 1000)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import type { AppProps } from 'next/app'
 import '../styles/main.scss'
 
-if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
-  import('../lib/axe.development').catch(console.error)
-}
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
