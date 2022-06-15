9dad0d3f545440a1e04bbdb51bf54eb81d875263
caminad <david@omg.lol>
2022-06-15 22:42:02 +0100

    Add Vitest (#2)

--- __previewjs__/Wrapper.tsx
+++ __previewjs__/Wrapper.tsx
@@ -3,6 +3,7 @@ import '../styles/main.scss'
 interface WrapperProps {
   readonly children: React.ReactNode
 }
+// ts-prune-ignore-next
 export function Wrapper(props: WrapperProps) {
   return <main>{props.children}</main>
 }
