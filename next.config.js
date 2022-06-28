639f2ccdb9798ee5e7d03f4943739e2ad4c16301
David Jones <david@omg.lol>
2022-06-28 18:12:10 +0100

    Update deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    browsersListForSwc: true,
+    legacyBrowsers: false,
+    newNextLinkBehavior: true,
+  },
 }
 
 module.exports = nextConfig
